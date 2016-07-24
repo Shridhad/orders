5981cef03998f18caa18fcaac217bf4204d3d074
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2016-07-24 22:32:56 +0530

    Addin mongoose and connecting to db

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 'use strict';
 
-const Hapi = require('hapi');
+const Hapi = require('hapi'),
+      config = require('./config/config'),
+      Mongoose = require('mongoose');
+
+
+Mongoose.connect('mongodb://' + config.database.host + '/' + config.database.db);
+const db = Mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function callback() {
+    console.log("Connection with database succeeded.");
+});
 
 const server = new Hapi.Server();
 server.connection({ port: 3333 });
