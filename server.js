862fdee51e2d895c41850e8085a7a625aa7f848a
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2016-07-24 22:35:43 +0530

    Refactoring

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 'use strict';
 
 const Hapi = require('hapi'),
-      config = require('./config/config'),
-      Mongoose = require('mongoose');
-
-
-Mongoose.connect('mongodb://' + config.database.host + '/' + config.database.db);
-const db = Mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function callback() {
-    console.log("Connection with database succeeded.");
-});
+      Db = require('./config/db');
 
 const server = new Hapi.Server();
 server.connection({ port: 3333 });
